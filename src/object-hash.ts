86953f4d71331268ab0874661819b7ad1aed0020
Balázs Németh <zsilbi@zsilbi.hu>
2025-03-03 17:20:02 +0100

    test(benchmarks): v1.1.5 vs v2.0.10

---
+++ src/object-hash.ts
@@ -0,0 +1,451 @@
+// Based on https://github.com/puleos/object-hash v3.0.0 (MIT)
+
+export interface HashOptions {
+    /**
+     * Function to determine if a key should be excluded from hashing.
+     * @optional
+     * @param key - The key to check for exclusion.
+     * @returns {boolean} - Returns true to exclude the key from hashing.
+     */
+    excludeKeys?: ((key: string) => boolean) | undefined;
+  
+    /**
+     * Specifies whether to exclude values from hashing, so that only the object keys are hashed.
+     * @optional
+     */
+    excludeValues?: boolean | undefined;
+  
+    /**
+     * Specifies whether to ignore objects of unknown type (not directly serialisable) when hashing.
+     * @optional
+     * @default false
+     */
+    ignoreUnknown?: boolean | undefined;
+  
+    /**
+     * A function that replaces values before they are hashed, which can be used to customise the hashing process.
+     * @optional
+     * @param value - The current value to be hashed.
+     * @returns {any} - The value to use for hashing instead.
+     */
+    replacer?: ((value: any) => any) | undefined;
+  
+    /**
+     * Specifies whether the 'name' property of functions should be taken into account when hashing.
+     * @optional
+     * @default false
+     */
+    respectFunctionNames?: boolean | undefined;
+  
+    /**
+     * Specifies whether properties of functions should be taken into account when hashing.
+     * @optional
+     * @default false
+     */
+    respectFunctionProperties?: boolean | undefined;
+  
+    /**
+     * Specifies whether to include type-specific properties such as prototype or constructor in the hash to distinguish between types.
+     * @optional
+     * @default false
+     */
+    respectType?: boolean | undefined;
+  
+    /**
+     * Specifies whether arrays should be sorted before hashing to ensure consistent order.
+     * @optional
+     * @default false
+     */
+    unorderedArrays?: boolean | undefined;
+  
+    /**
+     * Specifies whether Set and Map instances should be sorted by key before hashing to ensure consistent order.
+     * @optional
+     * @default true
+     */
+    unorderedObjects?: boolean | undefined;
+  
+    /**
+     * Specifies whether the elements of `Set' and keys of `Map' should be sorted before hashing to ensure consistent order.
+     * @optional
+     * @default false
+     */
+    unorderedSets?: boolean | undefined;
+  }
+  
+  // Defaults
+  const defaults: HashOptions = Object.freeze({
+    ignoreUnknown: false,
+    respectType: false,
+    respectFunctionNames: false,
+    respectFunctionProperties: false,
+    unorderedObjects: true,
+    unorderedArrays: false,
+    unorderedSets: false,
+    excludeKeys: undefined,
+    excludeValues: undefined,
+    replacer: undefined,
+  });
+  
+  /**
+   * Serialize any JS value into a stable, hashable string
+   * @param {object} object value to hash
+   * @param {HashOptions} options hashing options. See {@link HashOptions}.
+   * @return {string} serialized value
+   * @api public
+   */
+  export function objectHash(object: any, options?: HashOptions): string {
+    if (options) {
+      options = { ...defaults, ...options };
+    } else {
+      options = defaults;
+    }
+    const hasher = createHasher(options);
+    hasher.dispatch(object);
+    return hasher.toString();
+  }
+  
+  const defaultPrototypesKeys = Object.freeze([
+    "prototype",
+    "__proto__",
+    "constructor",
+  ]);
+  
+  function createHasher(options: HashOptions) {
+    let buff = "";
+    let context = new Map();
+    const write = (str: string) => {
+      buff += str;
+    };
+  
+    return {
+      toString() {
+        return buff;
+      },
+      getContext() {
+        return context;
+      },
+      dispatch(value: any): string | void {
+        if (options.replacer) {
+          value = options.replacer(value);
+        }
+        const type = value === null ? "null" : typeof value;
+        return this[type](value);
+      },
+      object(object: any): string | void {
+        if (object && typeof object.toJSON === "function") {
+          return this.object(object.toJSON());
+        }
+  
+        const objString = Object.prototype.toString.call(object);
+  
+        let objType = "";
+        const objectLength = objString.length;
+  
+        // '[object a]'.length === 10, the minimum
+        if (objectLength < 10) {
+          objType = "unknown:[" + objString + "]";
+        } else {
+          // '[object '.length === 8
+          objType = objString.slice(8, objectLength - 1);
+        }
+  
+        objType = objType.toLowerCase();
+  
+        let objectNumber = null;
+  
+        if ((objectNumber = context.get(object)) === undefined) {
+          context.set(object, context.size);
+        } else {
+          return this.dispatch("[CIRCULAR:" + objectNumber + "]");
+        }
+  
+        if (
+          typeof Buffer !== "undefined" &&
+          Buffer.isBuffer &&
+          Buffer.isBuffer(object)
+        ) {
+          write("buffer:");
+          return write(object.toString("utf8"));
+        }
+  
+        if (
+          objType !== "object" &&
+          objType !== "function" &&
+          objType !== "asyncfunction"
+        ) {
+          // @ts-ignore
+          if (this[objType]) {
+            // @ts-ignore
+            this[objType](object);
+          } else if (!options.ignoreUnknown) {
+            this.unkown(object, objType);
+          }
+        } else {
+          let keys = Object.keys(object);
+          if (options.unorderedObjects) {
+            keys = keys.sort();
+          }
+          let extraKeys = [] as readonly string[];
+          // Make sure to incorporate special properties, so Types with different prototypes will produce
+          // a different hash and objects derived from different functions (`new Foo`, `new Bar`) will
+          // produce different hashes. We never do this for native functions since some seem to break because of that.
+          if (options.respectType !== false && !isNativeFunction(object)) {
+            extraKeys = defaultPrototypesKeys;
+          }
+  
+          if (options.excludeKeys) {
+            keys = keys.filter((key) => {
+              return !options.excludeKeys!(key);
+            });
+            extraKeys = extraKeys.filter((key) => {
+              return !options.excludeKeys!(key);
+            });
+          }
+  
+          write("object:" + (keys.length + extraKeys.length) + ":");
+          const dispatchForKey = (key: string) => {
+            this.dispatch(key);
+            write(":");
+            if (!options.excludeValues) {
+              this.dispatch(object[key]);
+            }
+            write(",");
+          };
+          for (const key of keys) {
+            dispatchForKey(key);
+          }
+          for (const key of extraKeys) {
+            dispatchForKey(key);
+          }
+        }
+      },
+      array(arr: any, unordered: boolean): string | void {
+        unordered =
+          unordered === undefined ? options.unorderedArrays !== false : unordered; // default to options.unorderedArrays
+  
+        write("array:" + arr.length + ":");
+        if (!unordered || arr.length <= 1) {
+          for (const entry of arr) {
+            this.dispatch(entry);
+          }
+          return;
+        }
+  
+        // The unordered case is a little more complicated: since there is no canonical ordering on objects,
+        // i.e. {a:1} < {a:2} and {a:1} > {a:2} are both false,
+        // We first serialize each entry using a PassThrough stream before sorting.
+        // also: we can’t use the same context for all entries since the order of hashing should *not* matter. instead,
+        // we keep track of the additions to a copy of the context and add all of them to the global context when we’re done
+        const contextAdditions = new Map();
+        const entries = arr.map((entry: any) => {
+          const hasher = createHasher(options);
+          hasher.dispatch(entry);
+          for (const [key, value] of hasher.getContext()) {
+            contextAdditions.set(key, value);
+          }
+          return hasher.toString();
+        });
+        context = contextAdditions;
+        entries.sort();
+        return this.array(entries, false);
+      },
+      date(date: any) {
+        return write("date:" + date.toJSON());
+      },
+      symbol(sym: any) {
+        return write("symbol:" + sym.toString());
+      },
+      unkown(value: any, type: string) {
+        write(type);
+        if (!value) {
+          return;
+        }
+        write(":");
+        if (value && typeof value.entries === "function") {
+          return this.array(Array.from(value.entries()), true /* ordered */);
+        }
+      },
+      error(err: any) {
+        return write("error:" + err.toString());
+      },
+      boolean(bool: any) {
+        return write("bool:" + bool);
+      },
+      string(string: any) {
+        write("string:" + string.length + ":");
+        write(string);
+      },
+      function(fn: any) {
+        write("fn:");
+        if (isNativeFunction(fn)) {
+          this.dispatch("[native]");
+        } else {
+          this.dispatch(fn.toString());
+        }
+  
+        if (options.respectFunctionNames !== false) {
+          // Make sure we can still distinguish native functions
+          // by their name, otherwise String and Function will
+          // have the same hash
+          this.dispatch("function-name:" + String(fn.name));
+        }
+  
+        if (options.respectFunctionProperties) {
+          this.object(fn);
+        }
+      },
+      number(number: any) {
+        return write("number:" + number);
+      },
+      xml(xml: any) {
+        return write("xml:" + xml.toString());
+      },
+      null() {
+        return write("Null");
+      },
+      undefined() {
+        return write("Undefined");
+      },
+      regexp(regex: any) {
+        return write("regex:" + regex.toString());
+      },
+      uint8array(arr: any) {
+        write("uint8array:");
+        return this.dispatch(Array.prototype.slice.call(arr));
+      },
+      uint8clampedarray(arr: any) {
+        write("uint8clampedarray:");
+        return this.dispatch(Array.prototype.slice.call(arr));
+      },
+      int8array(arr: any) {
+        write("int8array:");
+        return this.dispatch(Array.prototype.slice.call(arr));
+      },
+      uint16array(arr: any) {
+        write("uint16array:");
+        return this.dispatch(Array.prototype.slice.call(arr));
+      },
+      int16array(arr: any) {
+        write("int16array:");
+        return this.dispatch(Array.prototype.slice.call(arr));
+      },
+      uint32array(arr: any) {
+        write("uint32array:");
+        return this.dispatch(Array.prototype.slice.call(arr));
+      },
+      int32array(arr: any) {
+        write("int32array:");
+        return this.dispatch(Array.prototype.slice.call(arr));
+      },
+      float32array(arr: any) {
+        write("float32array:");
+        return this.dispatch(Array.prototype.slice.call(arr));
+      },
+      float64array(arr: any) {
+        write("float64array:");
+        return this.dispatch(Array.prototype.slice.call(arr));
+      },
+      arraybuffer(arr: any) {
+        write("arraybuffer:");
+        return this.dispatch(new Uint8Array(arr));
+      },
+      url(url: any) {
+        return write("url:" + url.toString());
+      },
+      map(map: any) {
+        write("map:");
+        const arr = [...map];
+        return this.array(arr, options.unorderedSets !== false);
+      },
+      set(set: any) {
+        write("set:");
+        const arr = [...set];
+        return this.array(arr, options.unorderedSets !== false);
+      },
+      file(file: any) {
+        write("file:");
+        return this.dispatch([file.name, file.size, file.type, file.lastModfied]);
+      },
+      blob() {
+        if (options.ignoreUnknown) {
+          return write("[blob]");
+        }
+        throw new Error(
+          "Hashing Blob objects is currently not supported\n" +
+            'Use "options.replacer" or "options.ignoreUnknown"\n',
+        );
+      },
+      domwindow() {
+        return write("domwindow");
+      },
+      bigint(number: number) {
+        return write("bigint:" + number.toString());
+      },
+      /* Node.js standard native objects */
+      process() {
+        return write("process");
+      },
+      timer() {
+        return write("timer");
+      },
+      pipe() {
+        return write("pipe");
+      },
+      tcp() {
+        return write("tcp");
+      },
+      udp() {
+        return write("udp");
+      },
+      tty() {
+        return write("tty");
+      },
+      statwatcher() {
+        return write("statwatcher");
+      },
+      securecontext() {
+        return write("securecontext");
+      },
+      connection() {
+        return write("connection");
+      },
+      zlib() {
+        return write("zlib");
+      },
+      context() {
+        return write("context");
+      },
+      nodescript() {
+        return write("nodescript");
+      },
+      httpparser() {
+        return write("httpparser");
+      },
+      dataview() {
+        return write("dataview");
+      },
+      signal() {
+        return write("signal");
+      },
+      fsevent() {
+        return write("fsevent");
+      },
+      tlswrap() {
+        return write("tlswrap");
+      },
+    };
+  }
+  
+  const nativeFunc = "[native code] }";
+  const nativeFuncLength = nativeFunc.length;
+  
+  /** Check if the given function is a native function */
+  function isNativeFunction(f: any) {
+    if (typeof f !== "function") {
+      return false;
+    }
+    return (
+      Function.prototype.toString.call(f).slice(-nativeFuncLength) === nativeFunc
+    );
+  }
--- test/benchmarks.bench.ts
+++ test/benchmarks.bench.ts
@@ -1,30 +1,190 @@
 import { bench, describe } from "vitest";
 
 import { serialize } from "../src";
-import { digest as digestJS } from "../src/crypto/js";
-import { digest as digestNode } from "../src/crypto/node";
+import { objectHash } from "../src/object-hash";
 
 describe("benchmarks", () => {
-  describe("digest", async () => {
-    const input = "hello world";
-    const output = "uU0nuZNNPgilLlLX2n2r-sSE7-N6U4DukIj3rOLvzek";
-    const implementations = {
-      js: digestJS,
-      "node:crypto": digestNode,
-      subtleCrypto: subtleCrypto,
-      subtleCryptoWithBuffer: subtleCryptoWithNodeBuffer,
-    };
-    for (const [name, digest] of Object.entries(implementations)) {
-      if ((await digest(input)) !== output) {
-        throw new Error(
-          `digest implementation "${name}" is incorrect: ${await digest(input)}`,
-        );
-      }
-      // @ts-ignore (Promise<string>)
-      bench(name, () => {
-        return digest(input);
+  describe.only("serialize", () => {
+    // Options for v1
+    const hashOptions = { unorderedArrays: true, unorderedSets: true };
+
+    describe("simple object", () => {
+      const object = {
+        string: "test",
+        number: 42,
+        boolean: true,
+        nullValue: null,
+        undefinedValue: undefined,
+      };
+
+      bench("ohash v1.1.5", () => {
+        objectHash(object, hashOptions);
       });
-    }
+
+      bench("ohash v2.0.10", () => {
+        serialize(object);
+      });
+    });
+
+    describe("circular object", () => {
+      const object: any = {
+        string: "test",
+        number: 42,
+        boolean: true,
+        nullValue: null,
+        undefinedValue: undefined,
+      };
+
+      object.object = object;
+
+      bench("ohash v1.1.5", () => {
+        objectHash(object, hashOptions);
+      });
+
+      bench("ohash v2.0.10", () => {
+        serialize(object);
+      });
+    });
+
+    describe("array of 100 simple objects (by reference)", () => {
+      const object = {
+        string: "test",
+        number: 42,
+        boolean: true,
+        nullValue: null,
+        undefinedValue: undefined,
+      };
+
+      const array: any[] = [];
+
+      for (let i = 0; i < 100; i++) {
+        array.push(object);
+      }
+
+      bench("ohash v1.1.5", () => {
+        objectHash(array, hashOptions);
+      });
+
+      bench("ohash v2.0.10", () => {
+        serialize(array);
+      });
+    });
+
+    describe("complex object", () => {
+      const object = {
+        regex: /.*/,
+        url: new URL("https://example.com"),
+        symbol: Symbol("test"),
+        bigint: 9_007_199_254_740_991n,
+        set: new Set([1, 2, 3, {}]),
+        map: new Map<any, any>([["key", "value"]]),
+        array: [1, 2, "x", 3],
+      };
+
+      for (let i = 0; i < 50; i++) {
+        object.set.add(i);
+        object.set.add(Symbol(i));
+        object.set.add(`${i}`);
+        object.map.set(i, `${i}`);
+      }
+
+      bench("ohash v1.1.5", () => {
+        objectHash(object, hashOptions);
+      });
+
+      bench("ohash v2.0.10", () => {
+        serialize(object);
+      });
+    });
+
+    describe("dates", () => {
+      const object: any = {
+        dates: [],
+      };
+
+      for (let i = 0; i < 100; i++) {
+        object.dates.push(new Date(i));
+      }
+
+      bench("ohash v1.1.5", () => {
+        objectHash(object, hashOptions);
+      });
+
+      bench("ohash v2.0.10", () => {
+        serialize(object);
+      });
+    });
+
+    describe("typed arrays and buffer", () => {
+      const buffer = new ArrayBuffer(4096);
+
+      const object = {
+        int8Array: new Int8Array(buffer),
+        uint8Array: new Uint8Array([1, 2, 3]),
+        uint8ClampedArray: new Uint8ClampedArray([1, 2, 3]),
+        int16Array: new Int16Array(buffer),
+        uint16Array: new Uint16Array([1, 2, 3]),
+        int32Array: new Int32Array(buffer),
+        uint32Array: new Uint32Array([1, 2, 3]),
+        float32Array: new Float32Array(buffer),
+        float64Array: new Float64Array([1, 2, 3]),
+        bigInt64Array: new BigInt64Array(buffer),
+        bigUint64Array: new BigUint64Array([1n, 2n, 3n]),
+        buffer: Buffer.from("hello"),
+      };
+
+      bench("ohash v1.1.5", () => {
+        objectHash(object, hashOptions);
+      });
+
+      bench("ohash v2.0.10", () => {
+        serialize(object);
+      });
+    });
+
+    describe("classes/functions/nested objects", () => {
+      const object = {
+        nestedObject: { a: 1, b: 2 },
+        classInstance: new (class Test {
+          x = 1;
+        })(),
+        toJSONInstance: new (class Test {
+          toJSON() {
+            return [1, 2, 3];
+          }
+        })(),
+        formData: (() => {
+          const form = new FormData();
+          form.set("foo", "bar");
+          form.set("bar", "baz");
+          return form;
+        })(),
+        nativeFunction: Array,
+        customFunction: function sum(a: number, b: number) {
+          return a + b;
+        },
+        arrowFunction: (a: number, b: number) => a + b,
+        asyncFunction: async (a: number, b: number) => a + b,
+        generatorFunction: function* () {
+          yield 1;
+          yield 2;
+          yield 3;
+        },
+        asyncGeneratorFunction: async function* () {
+          yield 1;
+          yield 2;
+          yield 3;
+        },
+      };
+
+      bench("ohash v1.1.5", () => {
+        objectHash(object, hashOptions);
+      });
+
+      bench("ohash v2.0.10", () => {
+        serialize(object);
+      });
+    });
   });
 
   describe("serialize", () => {
@@ -81,29 +241,21 @@ describe("benchmarks", () => {
         .replace(/,/g, ", ");
       const objects = createBenchObjects(preset);
 
-      bench(title, () => {
-        serialize(objects);
+      describe(title, () => {
+        bench("ohash v1.1.5", () => {
+          objectHash(objects, {
+            unorderedArrays: true,
+            unorderedSets: true,
+          });
+        });
+
+        bench("ohash v2.0.10", () => {
+          serialize(objects);
+        });
       });
     }
   });
 });
-
-const encoder = new TextEncoder();
-
-function subtleCrypto(input: string) {
-  return crypto.subtle.digest("SHA-256", encoder.encode(input)).then((hash) => {
-    return btoa(String.fromCharCode(...new Uint8Array(hash)))
-      .replace(/=/g, "")
-      .replace(/\+/g, "-")
-      .replace(/\//g, "_");
-  });
-}
-
-function subtleCryptoWithNodeBuffer(input: string) {
-  return crypto.subtle
-    .digest("SHA-256", encoder.encode(input))
-    .then((hash) => Buffer.from(hash).toString("base64url"));
-}
 
 type BenchObjectPreset = {
   count: number;
@@ -193,15 +345,16 @@ function createBenchObjects({
       const circularObject = { ...object };
 
       circularObject.circular = object;
-      circularObject.map.set("object", object);
-      circularObject.set.add(object);
-      circularObject.set.add(circularObject);
+      /** These don't work with ohash v1 **/
+      // circularObject.map.set("object", object);
+      // circularObject.set.add(object);
+      // circularObject.set.add(circularObject);
 
-      object.circular = circularObject;
-      object.map.set("object", object);
-      object.map.set("clonedObject", circularObject);
-      object.set.add(object);
-      object.set.add(circularObject);
+      // object.circular = circularObject;
+      // object.map.set("object", object);
+      // object.map.set("clonedObject", circularObject);
+      // object.set.add(object);
+      // object.set.add(circularObject);
 
       for (let i = 0; i < 1000; i++) {
         object.set.add(i);
